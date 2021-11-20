97e9c9b20e3b3c52d91c037c13142e725dc62f9a
Merijn Hoogeveen <mail@merijn.pt>
2021-11-20 15:56:19 +0100

    Updates

--- index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from './app'
-
-const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<App />, wrapper) : false;
